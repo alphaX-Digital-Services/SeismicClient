13b0cc27bf378d8a54c5bb4f152f945d8bc86cf2
Frank Hartmann <frank.hartmann@alphax-ds.de>
2017-03-08 13:44:27 +0100

    Initial Commit with Client pointing to AWS instance

---
+++ background.js
@@ -0,0 +1,14 @@
+/* Regex-pattern to check URLs against. 
+   It matches URLs like: http[s]://[...]stackoverflow.com[...] */
+var urlRegex = /^http?:\/\/(?:[^./?#]+\.)?binary\.etxcapital\.com/;
+
+
+/* When the browser-action button is clicked... */
+chrome.browserAction.onClicked.addListener(function(tab) {
+    /*...check the URL of the active tab against our pattern and... */
+    if (urlRegex.test(tab.url)) {
+        /* ...if it matches, send a message specifying a callback too */
+        chrome.tabs.sendMessage(tab.id, { text: "report_back" },
+                                function(text) { alert(text)});
+    }
+});
---
+++ content.js
@@ -0,0 +1,98 @@
+/* Listen for messages */
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+    /* If the received message has the expected format... */
+    if (msg.text && (msg.text == "report_back")) {
+
+        // The node to be monitored
+        /*
+        var target = document.body;
+        (
+            new MutationObserver(function(mutationEventList){
+                console.log(mutationEventList)
+            })
+        )
+            .observe(document.body, {
+                childList: true,
+                attributes: true,
+                characterData: true,
+                subtree:true
+            });
+         Create an observer instance
+        var observer = new MutationObserver(function( mutations ) {
+            mutations.forEach(function( mutation ) {
+                alert(mutation);
+                var newNodes = mutation.addedNodes; // DOM NodeList
+                if( newNodes !== null ) { // If there are new nodes added
+                    var $nodes = $( newNodes ); // jQuery set
+                    $nodes.each(function() {
+                        var $node = $( this );
+                        console.log($node.html());
+                        if( $node.hasClass( "message" ) ) {
+                            // do something
+                        }
+                    });
+                }
+            });
+        });
+
+        // Configuration of the observer:
+        var config = {
+            attributes: true,
+            childList: true,
+            characterData: true,
+            subtree:true
+        };
+        // Pass in the target node, as well as the observer options
+        observer.observe(target, config);
+
+        // Later, you can stop observing
+        //observer.disconnect();
+
+
+        /* Call the specified callback, passing 
+           the web-pages DOM content as argument */
+
+
+
+
+        $("span#strike").on("DOMSubtreeModified", function() {
+            var rawInput = $("span#strike").html();
+            var data = $("span#strike").html().obfuscate();
+            var type = ($("span#asset.asset-value").html()).hashCode();
+            if(typeof rawInput !== "undefined" && rawInput !="" && !isNaN(data))
+            $.ajax({
+                type: "POST",
+                //url: "http://echo.jsontest.com/type/"+type+"/data/"+data+"/time/"+new Date().getTime(),
+                url: "https://35.157.238.180",
+                data: {"sensorId": type, "decimal" : data, "timestamp": new Date().getTime()},
+                success: function() {
+                    console.log("sent post data");
+                    console.log("Data: "+data);
+                    console.log("SensorID: "+type);
+                    console.log("Timestamp:"+new Date().getTime());
+                },
+                error: function() {
+                    console.error("could not send post data");
+                },
+                dataType: "json"
+            });
+        });
+        sendResponse("Watcher initialized");
+    }
+});
+
+String.prototype.hashCode = function() {
+    var hash = 0, i, chr, len;
+    if (this.length === 0) return hash;
+    for (i = 0, len = this.length; i < len; i++) {
+        chr   = this.charCodeAt(i);
+        hash  = ((hash << 5) - hash) + chr;
+        hash |= 0; // Convert to 32bit integer
+    }
+    return hash;
+};
+
+String.prototype.obfuscate = function() {
+    var float = parseFloat(this);
+    return float/1000;
+};
