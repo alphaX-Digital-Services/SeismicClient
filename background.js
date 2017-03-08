4fb5122b3c3082eeaf6bcc85e0c205c0e9346fc1
Frank Hartmann <frank.hartmann@alphax-ds.de>
2017-03-08 13:54:20 +0100

    test

--- background.js
+++ background.js
@@ -10,5 +10,4 @@ chrome.browserAction.onClicked.addListener(function(tab) {
         chrome.tabs.sendMessage(tab.id, { text: "report_back" },
                                 function(text) { alert(text)});
     }
-
 });
